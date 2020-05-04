dabc55f462f7a458a774f94e3ff3ca3662efc0b1
Damrongsak Reetanon <damrongs@gmail.com>
2020-05-04 14:39:59 +0700

    Update gen-ca.sh

--- gen-ca.sh
+++ gen-ca.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 
-export HOST=default
+HOST=`hostname`
 export CA_KEY=ca.key
 export CA_CERT=ca.crt
 
 echo "###### clear exiting file"
-rm -f ca* 
+rm -f ca*
 echo
 echo
 
@@ -14,5 +14,4 @@ echo "###### creating CA key and certificate"
 echo
 openssl genrsa -aes256 -passout pass:xpass -out ${CA_KEY} 4096
 openssl req -new -passin pass:xpass -x509 -days 3650 -key ${CA_KEY} -sha256 -out ${CA_CERT} -subj "/C=TH/ST=Bangkok/L=Bangkok/O=MyOrganization/OU=MyHome/CN=${HOST}"
-echo
 echo
