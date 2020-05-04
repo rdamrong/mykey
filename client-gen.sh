1f1876fccdea3313b40c3756eba61353c974a352
Damrongsak Reetanon <damrongs@gmail.com>
2020-05-04 14:13:20 +0700

    Update client-gen.sh

--- client-gen.sh
+++ client-gen.sh
@@ -21,7 +21,7 @@ openssl genrsa -out ${SHOST}-key.pem 4096
 openssl req -subj "/CN=$HOST" -sha256 -new -key ${SHOST}-key.pem -out ${SHOST}.csr
 echo subjectAltName = DNS:$HOST,IP:${HOSTIP},IP:127.0.0.1 >> extfile.cnf
 echo extendedKeyUsage = serverAuth >> extfile.cnf
-openssl x509 -passin pass:xpass -req -days 365 -sha256 -in ${SHOST}.csr -CA /etc/docker/tls/${CA_CERT} -CAkey /etc/docker/tls/${CA_KEY} \
+openssl x509 -passin pass:xpass -req -days 365 -sha256 -in ${SHOST}.csr -CA ${CA_CERT} -CAkey ${CA_KEY} \
   -CAcreateserial -out ${SHOST}-cert.pem -extfile extfile.cnf
 echo
 echo
