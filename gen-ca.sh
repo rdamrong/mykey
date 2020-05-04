6c706dfb75a6815e1c1f6532a1b461bfb0117eed
Damrongsak Reetanon <damrongs@gmail.com>
2020-05-04 14:07:28 +0700

    first

---
+++ gen-ca.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+export HOST=apple.example.com
+export HOSTIP=10.211.55.2
+export CA_KEY=ca.key
+export CA_CERT=ca.crt
+
+echo "###### clear exiting file"
+rm -f ca* 
+echo
+echo
+
+echo "###### creating CA key and certificate"
+echo
+openssl genrsa -aes256 -passout pass:xpass -out ${CA_KEY} 4096
+openssl req -new -passin pass:xpass -x509 -days 3650 -key ${CA_KEY} -sha256 -out ${CA_CERT} -subj "/C=TH/ST=Bangkok/L=Bangkok/O=MyOrganization/OU=MyHome/CN=${HOST}"
+echo
+echo
