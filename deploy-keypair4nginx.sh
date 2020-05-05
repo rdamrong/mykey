eba2ee4f5da60f98dfae88d986ba497e4872dac2
Damrongsak Reetanon <damrongs@gmail.com>
2020-05-05 19:31:10 +0700

    Update deploy-keypair4nginx.sh

--- deploy-keypair4nginx.sh
+++ deploy-keypair4nginx.sh
@@ -5,3 +5,4 @@ SHOST=`hostname -s `
 mkdir -p /etc/pki/nginx/private
 mv ${SHOST}-key.pem /etc/pki/nginx/private/server.key
 mv ${SHOST}-cert.pem /etc/pki/nginx/server.crt
+restorecon -R /etc/pki/nginx/
